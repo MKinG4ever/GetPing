f42cafeb4de0c55300bbbe093543fc37ad8f5b5d
NightFox <87749296+MKinG4ever@users.noreply.github.com>
2023-07-05 09:47:40 +0330

    Update GetPing_v0.2.py
    Upgraded functions:
    true_ip
    calc
    get_ping
    list_ping

--- GetPing_v0.1.py
+++ GetPing_v0.1.py
@@ -8,37 +8,35 @@ from sys import argv
 
 class GetPing:
     """
-        GetPing v0.1
+        GetPing v0.2
         author: NightFox
-        release: 7/4/2023
+        release: 7/5/2023
         see help as GetPing.help() for more information
     """
 
     def __init__(self):
-        self.ip = '127.0.0.1'  # default ip for monitoring mode feature (no effect on get_ping)
-        self.port = 80  # default port for monitoring mode feature (no effect on get_ping)
-        self.count = 3  # default count number
-        self.timeout = 3  # default timeout
+        # Default IP/PORT
+        self.ip = '127.0.0.1'  # default ip/port for monitoring mode feature (no effect on get_ping)
+        self.port = 80  # default port
+        # Default value
+        self.count = 5  # default count number (effect on list_ping)
+        self.timeout = 3  # default timeout (effect on ping, get_ping, list_ping)
+        self.version = 'GetPing v0.2'  # version information
 
     def __repr__(self):
-        return f'{self.repr}'  # rewrite Presentation
+        return f'{self.repr}ID:{id(self)}'  # rePresentation
 
+    # Class presentation
     @property
     def repr(self):
-        return f'GetPing v0.1\n{self.ip}[:{self.port}] \t (timeout={self.timeout} | count={self.count})'
+        return f'{self.version}\n{self.ip}[:{self.port}] \t (timeout={self.timeout} | count={self.count})\n'
 
-    # get ip s from file
+    # get/read the file
     @staticmethod
     def get_file(path):
         with open(path, 'r') as f:
             file = f.readlines()
         return file
-
-    # turn every line of file to <ip>:<port>
-    @staticmethod
-    def true_ip(line: str):
-        ip, port = line.strip().split(':')
-        return {'ip': ip, 'port': port}
 
     # interrupt function
     @staticmethod
@@ -57,7 +55,8 @@ class GetPing:
 
     # calculator function
     @staticmethod
-    def calc(pings):
+    def calc(ip, port, pings):
+        # calculating Part
         p = [i for i in pings if isinstance(i, float)]
         send = len(pings)
         received = len(p)
@@ -67,9 +66,20 @@ class GetPing:
             minimum = min(p)
             maximum = max(p)
             avg = sum(p) / len(p)
+        # presentation Part
         print(f'Send:{send} / Received:{received} / Lost:{lost}')
         print(f'avg:{avg:.3} / min:{minimum:.3} / max:{maximum:.3}\n') if p else print('- OUT OF RANGE\n')
-        return {'send': send, 'received': received, 'lost': lost, 'avg': avg, 'min': minimum, 'max': maximum}
+        return {'ip': ip, 'port': port, 'pings': pings, 'send': send, 'received': received, 'lost': lost,
+                'avg': avg, 'min': minimum, 'max': maximum}
+
+    # turn file to readable <ip>:<port> list
+    def true_ip(self, path):
+        file = self.get_file(path)
+        f = []
+        for line in file:
+            f.append(line.strip().split(':'))
+        # f = [line.strip().split(':') for line in self.get_file(path)]
+        return f
 
     # core function
     def ping(self, ip):
@@ -84,24 +94,28 @@ class GetPing:
             pings.append(p)
             print(f'{p:.3}') if isinstance(p, float) else print(f'{p}')
         # pings = [self.ping(ip) for _ in range(count)]
-        self.calc(pings)  # only presentation. no output
-        return {'ip': ip, 'port': port, 'pings': pings, 'count': count}
+        return self.calc(ip, port, pings)
 
     # ping ip s from list
     def list_ping(self, path):
-        file = self.get_file(path)
+        file = self.true_ip(path)
         result = []
         for line in file:
-            i = self.true_ip(line)
-            result.append(self.get_ping(i['ip'], i['port'], self.count))
+            r = self.get_ping(line[0], line[1], self.count)  # set default count number
+            result.append(r)
+        # result = [self.get_ping(line[0], line[1], self.count) for line in file]
         return result
 
     # reset default values
     def wizard(self):
-        self.ip = self.question('ip: ', '127.0.0.1')  # you can use 'self.ip' as answer
-        self.port = self.question('port: ', 80)
-        self.count = self.question('count number: ', 3)
-        self.timeout = self.question('timeout', 3)
+        # you can use '<IP>' or 'self.ip' for answer.
+        # Default IP/PORT
+        self.ip = self.question('ip: ', '127.0.0.1')  # default ip/port for monitoring mode
+        self.port = int(self.question('port: ', 80))
+        # Default values
+        self.count = int(self.question('count number: ', 5))
+        self.timeout = int(self.question('timeout: ', 3))
+        self.version = 'GetPing BetaVersion.'
 
     # feature
     def validation(self):
@@ -113,4 +127,9 @@ class GetPing:
         for module in GetPing().__dir__():
             if not module.startswith('_'):
                 n = f'GetPing().{module}'
-                print(f'GetPing(). {module:10} \t | {type(eval(n))}')
+                print(f'GetPing().{module:10} \t | {type(eval(n))}')
+        print('see github page for more.\n')
+        print('- true_ip() upgraded.')
+        print('- list_ping() upgraded.')
+        print('- calc() upgraded.')
+        print('- get_ping() upgraded.')
